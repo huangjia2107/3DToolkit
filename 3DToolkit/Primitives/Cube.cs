634e9784f02759945e31f24f389fa3f18653a826
huangjia <654664168@qq.com>
2018-12-03 14:10:12 +0800

    add Cube and Sphere

--- 3DToolkit/Primitives/Cube.cs
+++ 3DToolkit/Primitives/Cube.cs
@@ -5,10 +5,222 @@ using System.Text;
 using System.Windows.Media.Media3D;
 using ThreeDToolkit.Helps;
 using System.Windows.Media;
+using System.Windows;
 
 namespace ThreeDToolkit.Primitives
 {
     public class Cube : ModelVisual3D
     {
+        private Model3DGroup _modelGroup = null;
+
+        private GeometryModel3D _leftGM3D = null;
+        private GeometryModel3D _rightGM3D = null;
+        private GeometryModel3D _topGM3D = null;
+        private GeometryModel3D _bottomGM3D = null;
+        private GeometryModel3D _frontGM3D = null;
+        private GeometryModel3D _backGM3D = null;
+
+        #region Dependency Property
+
+        public static readonly DependencyProperty OriginProperty =
+            DependencyProperty.Register("Origin", typeof(Point3D), typeof(Cube), new UIPropertyMetadata(new Point3D(0, 0, 0), OnOriginPropertyChanged));
+        public Point3D Origin
+        {
+            get { return (Point3D)GetValue(OriginProperty); }
+            set { SetValue(OriginProperty, value); }
+        }
+        static void OnOriginPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Cube;
+            ctrl.ConstructModel3DGroup();
+        }
+
+        public static readonly DependencyProperty HeightProperty =
+            DependencyProperty.Register("Height", typeof(double), typeof(Cube), new UIPropertyMetadata(8d, OnHeightPropertyChanged, CoerceDistance), IsValidDoubleValue);
+        public double Height
+        {
+            get { return (double)GetValue(HeightProperty); }
+            set { SetValue(HeightProperty, value); }
+        }
+        static void OnHeightPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Cube;
+            ctrl.ConstructModel3DGroup();
+        }
+
+        public static readonly DependencyProperty WidthProperty =
+            DependencyProperty.Register("Width", typeof(double), typeof(Cube), new UIPropertyMetadata(8d, OnWidthPropertyChanged, CoerceDistance), IsValidDoubleValue);
+        public double Width
+        {
+            get { return (double)GetValue(WidthProperty); }
+            set { SetValue(WidthProperty, value); }
+        }
+        static void OnWidthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Cube;
+            ctrl.ConstructModel3DGroup();
+        }
+
+        public static readonly DependencyProperty LengthProperty =
+            DependencyProperty.Register("Length", typeof(double), typeof(Cube), new UIPropertyMetadata(8d, OnLengthPropertyChanged, CoerceDistance), IsValidDoubleValue);
+        public double Length
+        {
+            get { return (double)GetValue(LengthProperty); }
+            set { SetValue(LengthProperty, value); }
+        }
+        static void OnLengthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Cube;
+            ctrl.ConstructModel3DGroup();
+        }
+
+        private static object CoerceDistance(DependencyObject d, object value)
+        {
+            return Math.Max(0, (double)value);
+        }
+
+        private static bool IsValidDoubleValue(object value)
+        {
+            var d = (double)value;
+            return !(double.IsNaN(d) || double.IsInfinity(d));
+        }
+
+        public static readonly DependencyProperty SideMaterialsProperty =
+            DependencyProperty.Register("SideMaterials", typeof(MaterialGroup), typeof(Cube), new PropertyMetadata(null, OnSideMaterialsPropertyChanged));
+        public MaterialGroup SideMaterials
+        {
+            get { return (MaterialGroup)GetValue(SideMaterialsProperty); }
+            set { SetValue(SideMaterialsProperty, value); }
+        }
+        static void OnSideMaterialsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Cube;
+            ctrl.UpdateSideMaterials((MaterialGroup)e.NewValue);
+        }
+
+        #endregion
+
+        private void UpdateSideMaterials(MaterialGroup sideMaterials)
+        {
+            if (_modelGroup == null)
+                ConstructModel3DGroup();
+            else
+            {
+                for (int i = 0; i < _modelGroup.Children.Count; i++)
+                {
+                    var gm3D = _modelGroup.Children[i] as GeometryModel3D;
+
+                    if (sideMaterials == null)
+                        gm3D.Material = new DiffuseMaterial(Brushes.Gray);
+                    else
+                    {
+                        if (sideMaterials.Children.Count > i)
+                            gm3D.Material = sideMaterials.Children[i];
+                    }
+                }
+            }
+        }
+
+        private void ConstructModel3DGroup()
+        {
+            if (DoubleUtil.LessThanOrClose(Width, 0) || DoubleUtil.LessThanOrClose(Height, 0) || DoubleUtil.LessThanOrClose(Length, 0))
+                return;
+
+            //Model3DGroup
+            _modelGroup = new Model3DGroup();
+
+            //side
+            GenerateAllSideGeometryModel3D(Origin, Width, Height, Length);
+
+            this.Content = _modelGroup;
+        }
+
+        private GeometryModel3D GenerateSideGeometryModel3D(Point3D origin, IEnumerable<Point3D> point3Ds, int materialIndex)
+        {
+            var gm3D = new GeometryModel3D
+            {
+                Geometry = GenerateSideMesh(origin, new Point3DCollection(point3Ds)),
+                Material = new DiffuseMaterial(Brushes.Gray),
+                BackMaterial = new DiffuseMaterial(Brushes.LightGray)
+            };
+
+            if (SideMaterials != null && SideMaterials.Children.Count > materialIndex)
+                gm3D.Material = SideMaterials.Children[materialIndex];
+
+            return gm3D;
+        }
+
+        //start point3D is left-back-bottom corner
+        private void GenerateAllSideGeometryModel3D(Point3D origin, double width, double height, double length)
+        {
+            //left
+            _leftGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(0, 0, 0), new Point3D(0, 0, width), new Point3D(0, height, 0), new Point3D(0, height, width) }), 0);
+
+            //right
+            _rightGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(length, 0, width), new Point3D(length, 0, 0), new Point3D(length, height, width), new Point3D(length, height, 0) }), 1);
+
+            //top
+            _topGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(0, height, width), new Point3D(length, height, width), new Point3D(0, height, 0), new Point3D(length, height, 0) }), 2);
+
+            //bottom
+            _bottomGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(length, 0, width), new Point3D(0, 0, width), new Point3D(length, 0, 0), new Point3D(0, 0, 0) }), 3);
+
+            //front
+            _frontGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(0, 0, width), new Point3D(length, 0, width), new Point3D(0, height, width), new Point3D(length, height, width) }), 4);
+
+            //back
+            _backGM3D = GenerateSideGeometryModel3D(origin,
+                new Point3DCollection(new Point3D[] { new Point3D(length, 0, 0), new Point3D(0, 0, 0), new Point3D(length, height, 0), new Point3D(0, height, 0) }), 5);
+
+            _modelGroup.Children.Add(_leftGM3D);
+            _modelGroup.Children.Add(_rightGM3D);
+            _modelGroup.Children.Add(_topGM3D);
+            _modelGroup.Children.Add(_bottomGM3D);
+            _modelGroup.Children.Add(_frontGM3D);
+            _modelGroup.Children.Add(_backGM3D);
+        }
+
+        /*  add point3d by 0->1->2->3
+            triangle by 0->1->2  2->1->3
+        
+            (2)            (3)
+             @--------------@
+             |              |
+             |              |
+             |              |
+             |              |
+             @--------------@
+             (0)          (1)
+        */
+        private MeshGeometry3D GenerateSideMesh(Point3D origin, Point3DCollection point3Ds)
+        {
+            var mesh = new MeshGeometry3D();
+
+            foreach (var p3D in point3Ds)
+            {
+                mesh.Positions.Add(new Point3D(p3D.X + origin.X, p3D.Y + origin.Y, p3D.Z + origin.Z));
+            }
+
+            mesh.TriangleIndices.Add(0);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(2);
+
+            mesh.TriangleIndices.Add(2);
+            mesh.TriangleIndices.Add(1);
+            mesh.TriangleIndices.Add(3);
+
+            mesh.TextureCoordinates.Add(new Point(0, 1));
+            mesh.TextureCoordinates.Add(new Point(1, 1));
+            mesh.TextureCoordinates.Add(new Point(0, 0));
+            mesh.TextureCoordinates.Add(new Point(1, 0));
+
+            return mesh;
+        }
+
     }
 }
--- 3DToolkit/Primitives/Sphere.cs
+++ 3DToolkit/Primitives/Sphere.cs
@@ -5,10 +5,148 @@ using System.Text;
 using System.Windows.Media.Media3D;
 using ThreeDToolkit.Helps;
 using System.Windows.Media;
+using System.Windows;
 
 namespace ThreeDToolkit.Primitives
 {
     public class Sphere : ModelVisual3D
     {
+        private const int Stacks = 20;
+        private const int Slices = 20;
+
+        private Model3DGroup _modelGroup = null;
+        private GeometryModel3D _geometryModel3D = null;
+
+        #region Dependency Property
+
+        public static readonly DependencyProperty OriginProperty =
+            DependencyProperty.Register("Origin", typeof(Point3D), typeof(Sphere), new UIPropertyMetadata(new Point3D(0, 0, 0), OnOriginPropertyChanged));
+        public Point3D Origin
+        {
+            get { return (Point3D)GetValue(OriginProperty); }
+            set { SetValue(OriginProperty, value); }
+        }
+        static void OnOriginPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Sphere;
+            ctrl.UpdateGeometry();
+        }
+
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.Register("Radius", typeof(double), typeof(Sphere), new PropertyMetadata(4d, OnRadiusPropertyChanged, CoerceDistance), IsValidDoubleValue);
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+        static void OnRadiusPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Sphere;
+            ctrl.UpdateGeometry();
+        }
+
+        private static object CoerceDistance(DependencyObject d, object value)
+        {
+            return Math.Max(0, (double)value);
+        }
+
+        private static bool IsValidDoubleValue(object value)
+        {
+            var d = (double)value;
+            return !(double.IsNaN(d) || double.IsInfinity(d));
+        }
+
+        public static readonly DependencyProperty MaterialProperty =
+            DependencyProperty.Register("Material", typeof(Material), typeof(Sphere), new PropertyMetadata(new DiffuseMaterial(Brushes.Gray), OnMaterialPropertyChanged));
+        public Material Material
+        {
+            get { return (Material)GetValue(MaterialProperty); }
+            set { SetValue(MaterialProperty, value); }
+        }
+        static void OnMaterialPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as Sphere;
+            ctrl.UpdateMaterial(ctrl._geometryModel3D, (Material)e.NewValue);
+        }
+
+        #endregion
+
+        private void UpdateGeometry()
+        {
+            if (_modelGroup == null)
+                ConstructModel3DGroup();
+            else
+                _geometryModel3D.Geometry = GenerateMeshGeometry3D(Origin, Radius, Stacks, Slices);
+        }
+
+        private void UpdateMaterial(GeometryModel3D geometryModel3D, Material material)
+        {
+            if (geometryModel3D == null)
+                ConstructModel3DGroup();
+            else
+                geometryModel3D.Material = material;
+        }
+
+        private void ConstructModel3DGroup()
+        {
+            if (DoubleUtil.LessThanOrClose(Radius, 0))
+                return;
+
+            //Model3DGroup
+            _modelGroup = new Model3DGroup();
+
+            //Bottom
+            _geometryModel3D = new GeometryModel3D
+            {
+                Geometry = GenerateMeshGeometry3D(Origin, Radius, Stacks, Slices),
+                Material = Material
+            };
+
+            _modelGroup.Children.Add(_geometryModel3D);
+
+            this.Content = _modelGroup;
+        }
+
+        /*              
+         * Horizontal Direction  (0,0,-z) -> (-x,0,0)  -> (0,0,z) -> (x,0,0)
+         * Vertical Direction    (0,y,0)  -> (0,-y,0)
+         * 
+         * phi [0-2*Math.PI]   theta [0-Math.PI]
+         */
+        private MeshGeometry3D GenerateMeshGeometry3D(Point3D origin, double radius, int stacks, int slices)
+        {
+            var mesh = new MeshGeometry3D();
+
+            double phi, theta, x, y, z;
+
+            for (int i = 0; i <= slices; i++)
+            {
+                theta = Math.PI * i / slices;
+                y = radius * Math.Cos(theta);
+
+                for (int j = 0; j <= stacks; j++)
+                {
+                    phi = 2 * Math.PI * j / stacks;
+                    x = -radius * Math.Sin(theta) * Math.Sin(phi);
+                    z = -radius * Math.Sin(theta) * Math.Cos(phi);
+
+                    mesh.Positions.Add(new Point3D(x + origin.X, y + origin.Y, z + origin.Z));
+                    mesh.TextureCoordinates.Add(new Point((double)j / stacks, (double)i / slices));
+
+                    if (i > 0 && j < stacks)
+                    {
+                        mesh.TriangleIndices.Add(i * (stacks + 1) + j);
+                        mesh.TriangleIndices.Add(i * (stacks + 1) + j + 1);
+                        mesh.TriangleIndices.Add((i - 1) * (stacks + 1) + j);
+
+                        mesh.TriangleIndices.Add((i - 1) * (stacks + 1) + j);
+                        mesh.TriangleIndices.Add(i * (stacks + 1) + j + 1);
+                        mesh.TriangleIndices.Add((i - 1) * (stacks + 1) + j + 1);
+                    }
+                }
+            }
+
+            return mesh;
+        }
     }
 }
